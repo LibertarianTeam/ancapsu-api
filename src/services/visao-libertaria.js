96580f6ba0377fcaf9d8620aa071231ab6b7ef14
paulloclara <paulloclara@gmail.com>
2020-07-05 18:28:35 -0300

    :fire: Set "max" to 12 by default

--- src/services/visao-libertaria.js
+++ src/services/visao-libertaria.js
@@ -2,32 +2,31 @@ const axios = require('../plugins/axios');
 const camelcaseObjectDeep = require('camelcase-object-deep');
 
 function buildQuery(options) {
-  return Object.keys(options).reduce(
-    (query, key, index) =>
-      `${query}${index !== 0 ? '&' : ''}${key}=${options[key]}`,
-    ''
-  );
+  return Object.keys(options).reduce((query, key, index) => {
+    const value = options[key];
+
+    return `${query}${index !== 0 ? '&' : ''}${key}=${value}`;
+  }, '');
 }
 
 module.exports = {
   async list({ type, ini = 0, max = 12 } = {}) {
     const query = buildQuery({ ini, max });
-    const responseData = (await axios.get(`/${type}/list?${query}`)).data;
+    const response = await axios.get(`/${type}/list?${query}`);
 
-    return camelcaseObjectDeep(responseData);
+    return camelcaseObjectDeep(response.data);
   },
-  async category({ type, categ = 'news', ini = 0, max = 10 } = {}) {
+  async category({ type, categ = 'news', ini = 0, max = 12 } = {}) {
     const query = buildQuery({ categ, ini, max });
-    const responseData = (await axios.get(`/${type}/bycategory?${query}`)).data;
+    const response = await axios.get(`/${type}/bycategory?${query}`);
 
-    return camelcaseObjectDeep(responseData);
+    return camelcaseObjectDeep(response.data);
   },
-  async search({ type, search = '', ini = 0, max = 10 } = {}) {
+  async search({ type, search = '', ini = 0, max = 12 } = {}) {
     const query = buildQuery({ ini, max });
     const body = { SearchString: search };
-    const responseData = (await axios.post(`/${type}/search?${query}`, body))
-      .data;
+    const response = await axios.post(`/${type}/search?${query}`, body);
 
-    return camelcaseObjectDeep(responseData);
+    return camelcaseObjectDeep(response.data);
   },
 };
